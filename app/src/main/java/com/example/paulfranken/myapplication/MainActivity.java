4e1389a1e69b604100611c6dc06f52f773820d13
BuildTools <unconfigured@null.spigotmc.org>
2018-02-25 15:17:52 +0100

    FIX

--- app/src/main/java/com/example/paulfranken/myapplication/MainActivity.java
+++ app/src/main/java/com/example/paulfranken/myapplication/MainActivity.java
@@ -92,6 +92,149 @@ public class MainActivity extends AppCompatActivity implements OnClickListener,V
     //Der Context für die gesamte Application wird geschpeichert. Zum Beispiel für Toasts
     //----------------------------------------------Layout, Werbung und Context speichern----------------------------------------------
 
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        context=getApplicationContext();
+
+
+
+        //Speichern des Layouts und festlegen der Toolbar, Speichern des ApplicationContexts
+
+
+
+
+
+
+
+
+
+        //--------------------------------------------------------Swipe Layout---------------------------------------------------------
+
+        l=(SwipeRefreshLayout)findViewById(R.id.swipe);
+
+        l.setColorSchemeResources(R.color.f1,R.color.f4,R.color.f7);//Farben festlegen
+
+        l.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+
+            public void onRefresh() {
+
+                l.setRefreshing(true);
+
+                (new Handler()).postDelayed(new Runnable() {
+
+                    @Override
+
+                    public void run() {
+
+                        aktualisieren();
+
+                        //Der Vertretungsplan wird erneut überprüft und abgeglichen
+
+                    }
+
+                },3000);
+
+
+
+            }
+
+        });
+
+        //Das Swipe Layout(Main Layout wird in der Variable l geschpeichert. Die Farben werden festgelegt, es wird ein
+
+        //OnRefreshListener festgelegt der bei aktivierung die Methode aktualisieren aufruft
+
+        //--------------------------------------------------------Swipe Layout---------------------------------------------------------
+
+
+
+
+
+        //------------------------------Mehrere Methoden die zum Start benötigt werden, werden aufgerufen------------------------------
+
+        speichernlayouts();
+
+        //In der ArrayList alleStunden wird jedes TextView geschpeichert und mit Listenern ausgestattet
+
+
+
+        setTage();
+
+        //Alles TextViews(Stunden) bekommen zugewiesen welchen Tag sie haben
+
+        setzeZeiten();
+
+        //Den TextViews der 1.Zeile wird ein Text zugewiesen, der besagt zu welcher Uhrzeit diese Stunde beginnt
+
+
+
+        laden_einstellungen();
+
+        //Es wird aus den SharedPreferences geladen welche Klasse eingestellt wurde
+
+
+
+        Laden();
+
+        //Beim Start werden alle Daten aus den SharedPrefreferneces geladen und auf den TextViews wieder angzeigt
+
+        widget_speichern();
+
+        //Alle Facher und deren Räume werden in SharedPreferences geschpeichert um sie dem Widget zur Verfügung zu stellen
+
+        WidgetProvider.updateWidget(context);
+
+        //Das Widget wird mit neuen Informationen geupdatet
+
+        //------------------------------Mehrere Methoden die zum Start benötigt werden, werden aufgerufen------------------------------
+
+
+
+        //------------------------------------Der Werbung Block wird geschpeichert und eingestellt-------------------------------------
+
+        adView = findViewById(R.id.testadview);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        adView.loadAd(adRequest);
+
+        //Das AdView wird geschpeichert und eigestellt
+
+        SharedPreferences settings=getSharedPreferences("ADPREFS",0);
+
+        String addys= settings.getString("adwords",null);
+
+        if(addys != null){
+
+            if (addys.equals("aus")) {
+
+                adView.setVisibility(View.GONE);
+
+            }
+
+        }
+
+        //Es wir überpüft ob die Werbung mit dem Code ausgeschaltet wurde
+
+        //------------------------------------Der Werbung Block wird geschpeichert und eingestellt-------------------------------------
+
+
+
+
+
+    }
 
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
